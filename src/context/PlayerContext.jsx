ec6c87136b4132dfce3b9914e9764468dd6db7fb
kumar543217 <dee96872.com>
2024-09-03 22:58:48 +0530

    i have add functoin to sheekbar update

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -23,30 +23,58 @@ const PlayerContextProvider = (props) => {
         }
     })
 
-    const play = () =>{
+    const play = () => {
         audioRef.current.play();
         setPlayStatus(true);
     }
 
-    const pause = () =>{
+    const pause = () => {
         audioRef.current.pause();
         setPlayStatus(false);
     }
 
-    useEffect(()=>{
-        audioRef.current.ontimeupdate = () =>{
+    useEffect(() => {
+        audioRef.current.ontimeupdate = () => {
+
+            seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100)) + "%"
             setTime({
                 currentTime: {
-                    second: 0,
-                    minute: 0
+                    second: Math.floor(audioRef.current.currentTime % 60),
+                    minute: Math.floor(audioRef.current.currentTime / 60),
                 },
                 totalTime: {
-                    second: 0,
-                    minute: 0,
+                    second: Math.floor(audioRef.current.duration % 60),
+                    minute: Math.floor(audioRef.current.duration / 60),
                 }
             })
         }
-    },[audioRef])
+    }, [audioRef])
+
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const previous = async () => {
+        if (track.id > 0) {
+            await setTrack(songsData[track.id - 1])
+            audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const next = async () => {
+        if (track.id < songsData.length - 1) {
+            await setTrack(songsData[track.id + 1])
+            audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const seekSong = async (e) => {
+      audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth)* audioRef.current.duration)
+    }
 
     const contextValue = {
         audioRef,
@@ -55,7 +83,11 @@ const PlayerContextProvider = (props) => {
         track, setTrack,
         playStatus, setPlayStatus,
         time, setTime,
-        play, pause
+        play, pause,
+        playWithId,
+        previous,
+        next,
+        seekSong,
     }
 
     return (
